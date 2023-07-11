564c9ad34cbb5d8c679adb9a52f58efcac0271aa
ermesand <dafka100@gmail.com>
2023-07-11 19:09:54 +0300

    change: валидация добавления карточки

--- src/components/AddPlacePopup.js
+++ src/components/AddPlacePopup.js
@@ -4,11 +4,26 @@ import PopupWithForm from "./PopupWithForm.js";
 function AddPlacePopup(props) {
   const [title, setTitle] = React.useState("");
   const [link, setLink] = React.useState("");
+  const [isValidName, setIsValidName] = React.useState(false);
+  const [isValidSrc, setIsValidSrc] = React.useState(false);
+  
   function handleTitle(e) {
     setTitle(e.target.value);
+    if (e.target.value.length >= 2 && e.target.value.length <= 30) {
+        setIsValidName(true);
+      } else {
+        setIsValidName(false);
+      }
   }
   function handleLink(e) {
     setLink(e.target.value);
+    const regexp =
+      /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+    if (regexp.test(e.target.value)) {
+        setIsValidSrc(true);
+    } else {
+        setIsValidSrc(false);
+    }
   }
   function handleSubmit(e) {
     e.preventDefault();
@@ -25,6 +40,7 @@ function AddPlacePopup(props) {
       isOpen={props.isOpen}
       onClose={props.onClose}
       onSubmit={handleSubmit}
+      isValid={isValidName && isValidSrc}
     >
       <input
         className="popup__input popup__input_type_title"
